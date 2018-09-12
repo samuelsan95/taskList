7242f899627c545bac88c6732ec178d0108f0710
Samuel <samuel.sanchez@uxsmobile.com>
2018-09-12 16:47:49 +0200

    add badge in total tasks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,8 @@ class Navigation extends Component {
         return (
             <nav className="navbar navbar-dark bg-dark">
                 <a href="" className="text-white">
-                { this.props.title } - { this.props.total }
+                { this.props.title } 
+                <span className="badge badge-pill badge-light ml-2"> { this.props.total }</span>
                 </a>
             </nav>
         )
